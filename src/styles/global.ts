dc7c2321a700e4ae286fb4439c20031dd0325c56
Marden Guilherme Ribeiro Melo <mardenmelo@Marden-MBP.home>
2022-02-27 19:55:48 -0300

    More components created

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,10 +4,11 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #e52e4d;
+        --green: #33cc95;
         --blue: #5429cc;
         --blue-light: #6933ff;
         --text-title: #363f5f;
-        --text-body: #f0f2f5;
+        --text-body: #969CB3;
         --shape: #ffffff;
     }
 
